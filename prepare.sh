ffcc5728116f2cac69b79deb68557a8a39d3c96c
Pavel Petracek <petrapa6@fel.cvut.cz>
2022-06-15 11:11:07 +0200

    add pdf

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,7 @@
 
 "gs" -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=build/cv.pdf build/main.pdf
 
-ln -s build/cv.pdf cv.pdf
+cp build/cv.pdf cv.pdf
 
 cd about
 cp paragraph.md paragraph.txt
