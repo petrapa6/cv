31a0deb0d05e2ea9d10bebfbf7c6d407b0684ce3
Pavel Petracek <petracekpav@gmail.com>
2026-01-02 10:48:39 +0100

    update affiliations

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Welcome to my CV repo!
 You will find 2 CVs here:
-- A [**1-page CV**](https://github.com/petrapa6/cv/raw/master/cv.pdf) summarizing my career
-- or my [**academic CV**](https://github.com/petrapa6/cv/raw/master/academic_cv.pdf) with my academic record, list of publications, and more.
+- A [**1-page CV**](https://github.com/petrapa6/cv/raw/master/cv.pdf) summarizing my career or
+- my [**academic CV**](https://github.com/petrapa6/cv/raw/master/academic_cv.pdf) with my full academic record, list of publications, and more.
--- prepare.sh
+++ prepare.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 # screen, ebook, prepress, printer, default
 
-# bash process_bib.sh
-
 "gs" -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=academic/build/cv.pdf academic/build/main.pdf
 "gs" -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=industrial/build/cv.pdf industrial/build/main.pdf
 
 cp academic/build/cv.pdf academic_cv.pdf
 cp industrial/build/cv.pdf cv.pdf
-
-# cd about
-# cp paragraph.md paragraph.txt
